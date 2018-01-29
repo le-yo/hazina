c0945a5a56375a33869bca548e7babee61ca8deb
Le-Yo <le-yo@le-yo.com>
2018-01-29 21:28:06 +0300

    added fields to message outbox table

---
+++ database/migrations/2018_01_25_185513_add_fields_to_outbox_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToOutboxTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('outbox', function(Blueprint $table)
+        {
+            $table->integer('status')->after('message')->default(0);
+            $table->integer('reminder_id')->after('message')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('outbox', function(Blueprint $table)
+        {
+            $table->dropColumn('status');
+            $table->dropColumn('reminder_id');
+        });
+    }
+}
