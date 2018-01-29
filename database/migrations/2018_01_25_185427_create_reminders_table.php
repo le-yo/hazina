eb8a1d45cb927c50df068ddd92ed5684ca9d2fc3
Le-Yo <le-yo@le-yo.com>
2018-01-29 21:27:33 +0300

    added reminders table seeder

---
+++ database/migrations/2018_01_25_185427_create_reminders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRemindersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reminders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug');
+            $table->longText('message');
+            $table->string('type')->nullable();
+            $table->string('days_to')->nullable();
+            $table->string('days_overdue')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reminders');
+    }
+}
