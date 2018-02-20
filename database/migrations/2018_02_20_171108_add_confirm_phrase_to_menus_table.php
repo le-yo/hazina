a85cac2be7a50989361046e0dc8a4cd7718ef7a4
Le-Yo <le-yo@le-yo.com>
2018-02-20 17:30:34 +0300

    added confirmation_title to menus table

---
+++ database/migrations/2018_02_20_171108_add_confirm_phrase_to_menus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConfirmPhraseToMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('menus', function(Blueprint $table)
+        {
+            $table->string('confirmation_title')->after('confirmation_message')->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('outbox', function(Blueprint $table)
+        {
+            $table->dropColumn('confirmation_title');
+        });
+    }
+}
