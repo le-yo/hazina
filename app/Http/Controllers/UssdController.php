8e7d4ddefd166371625e8f8b3a329fac02622c4f
Le-Yo <le-yo@le-yo.com>
2018-05-02 10:10:01 +0300

    added messaging for new loan applications

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -1000,6 +1000,11 @@ class UssdController extends Controller
             self::sendResponse($error, 2, $user);
             exit;
         } else {
+            $now = Carbon::now()->toDateString();
+            $new_loan = $now.": New loan application from ".$user->name." amount ".number_format($loan,2).".";
+            $notify->sendSms("254707773267", $new_loan);
+            $notify->sendSms("254705099230", $new_loan);
+            $notify->sendSms("254728355429", $new_loan);
             return TRUE;
         }
     }
