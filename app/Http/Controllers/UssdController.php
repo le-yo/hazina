3470a3e3f874a467e32f0ef372c30c71f42f87f2
Le-Yo <le-yo@le-yo.com>
2018-05-12 10:43:42 +0300

    added Ludovic's number

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -1004,7 +1004,7 @@ class UssdController extends Controller
             $new_loan = $now.": New loan application from ".$user->name." amount ".number_format($loan,2).".";
             $notify->sendSms("254707773267", $new_loan);
             $notify->sendSms("254705099230", $new_loan);
-            $notify->sendSms("254728355429", $new_loan);
+            $notify->sendSms("254797654085", $new_loan);
             return TRUE; 
         }
     }
