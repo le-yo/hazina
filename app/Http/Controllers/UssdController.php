82c6158fac0f3765438007f50e2d2e6e90143c68
Le-Yo <le-yo@le-yo.com>
2018-05-12 11:30:31 +0300

    added ludic

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -1004,8 +1004,9 @@ class UssdController extends Controller
             $new_loan = $now.": New loan application from ".$user->name." amount ".number_format($loan,2).".";
             $notify->sendSms("254707773267", $new_loan);
             $notify->sendSms("254705099230", $new_loan);
+            $notify->sendSms("254728355429", $new_loan);
             $notify->sendSms("254797654085", $new_loan);
-            return TRUE; 
+            return TRUE;
         }
     }
 
