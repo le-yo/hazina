df174586e8e690af1f4b7e55d3d808b2248b284e
Le-Yo <le-yo@le-yo.com>
2018-09-14 00:35:05 +0300

    Fixing Hazina issues

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,17 +13,17 @@ class UserTableSeeder extends Seeder
     {
         //
         $role_admin = \App\Role::where('name', 'admin')->first();
-        $role_manager  = \App\Role::where('name', 'manager')->first();
+        $role_manager  = \App\Role::where('name', 'user')->first();
         $admin = new \App\User();
         $admin->name = 'Admin';
-        $admin->email = 'admin@unilimited.devs.mobi';
-        $admin->password = bcrypt('unicredit696');
+        $admin->email = 'admin@hazina.devs.mobi';
+        $admin->password = bcrypt('password');
         $admin->save();
         $admin->roles()->attach($role_admin);
         $manager = new \App\User();
-        $manager->name = 'Manager';
-        $manager->email = 'manager@unicredit.devs.mobi';
-        $manager->password = bcrypt('unicredit696');
+        $manager->name = 'user';
+        $manager->email = 'user@hazina.devs.mobi';
+        $manager->password = bcrypt('password');
         $manager->save();
         $manager->roles()->attach($role_manager);
     }
