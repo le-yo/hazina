8298c0dd362fce5b727fabd194eb582a0b260369
Le-Yo <le-yo@le-yo.com>
2018-09-14 01:01:35 +0300

    Removed title

--- resources/views/centaur/layout.blade.php
+++ resources/views/centaur/layout.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>UniCredit | @yield('title')</title>
-
+        <title> @yield('title')</title>
+ 
         <!-- Bootstrap - Latest compiled and minified CSS -->
         <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}">
