d3434fe12d424f53e5d6e847c151725a7b34a1b2
Le-Yo <le-yo@le-yo.com>
2018-09-14 00:43:24 +0300

    homepage works fine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hazina Microfinance
+                    Hazina Microfinance  
                 </div>
 
                 {{--<div class="links">--}}
