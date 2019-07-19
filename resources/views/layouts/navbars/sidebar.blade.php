1d76bb10ab42377f5197f949f9bc670481ac717b
Le-Yo <le-yo@le-yo.com>
2019-07-19 08:12:29 +0300

    Fixing stuff

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -23,10 +23,10 @@
                         <h6 class="text-overflow m-0">{{ __('Welcome!') }}</h6>
                     </div>
                     {{--<a href="{{ route('profile.edit') }}" class="dropdown-item">--}}
-                    <a href="#" class="dropdown-item">
-                        <i class="ni ni-single-02"></i>
-                        <span>{{ __('My profile') }}</span>
-                    </a>
+                    {{--<a href="#" class="dropdown-item">--}}
+                        {{--<i class="ni ni-single-02"></i>--}}
+                        {{--<span>{{ __('My profile') }}</span>--}}
+                    {{--</a>--}}
                     <a href="#" class="dropdown-item">
                         <i class="ni ni-settings-gear-65"></i>
                         <span>{{ __('Settings') }}</span>
